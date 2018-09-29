9ff17cf147d0bbb7801bdb51460dec6fb5d4f994
Daniel <dcamajaig@gmail.com>
2018-09-29 10:44:55 -0600

    cambios
    cambios

--- app/src/main/java/petto/com/petto/Main3Activity.java
+++ app/src/main/java/petto/com/petto/Main3Activity.java
@@ -1,16 +1,15 @@
 package petto.com.petto;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -33,7 +32,11 @@ public class Main3Activity extends AppCompatActivity {
         boton_publicar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String titulo = titulop.getText().toString();
+                Intent intent = new Intent(Main3Activity.this, Main2Activity.class);
+                startActivity(intent);
+                Toast.makeText(Main3Activity.this, "Datos enviados", Toast.LENGTH_SHORT).show();
+
+            /*    String titulo = titulop.getText().toString();
                 int precio = Integer.parseInt(preciop.getText().toString());
                 String descripcion = descripcionp.getText().toString();
 
@@ -43,8 +46,8 @@ public class Main3Activity extends AppCompatActivity {
                 hopperUpdates.put("descrpcion", descripcion);
 
 
-                mDatabasep.child("ususarios").push().setValue(hopperUpdates);
-
+              mDatabasep.child("ususarios").push().setValue(hopperUpdates);
+               */
             }
         });
 
